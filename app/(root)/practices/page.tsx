ca9fcb880323e4653683313e29a5d0ed097c3fb3
Mohammed Abdallah <mo8837145@gmail.com>
2025-12-15 01:18:34 +0300

    Update page.tsx

--- app/(root)/practices/page.tsx
+++ app/(root)/practices/page.tsx
@@ -8,7 +8,7 @@ function page() {
 
   return (
     <>
-    <HeroSection image={"/Heropractices.png"} text={"Practices & Sectors"} className={"h-120"} textClass='w-full' aboutClass="justify-center w-full"/>
+    <HeroSection image={"/HeroPractices.png"} text={"Practices & Sectors"} className={"h-120"} textClass='w-full' aboutClass="justify-center w-full"/>
     <Header text='The firm thrives on a culture of cooperation and shared contribution, ensuring that expertise
         and experience is shared across departments whenever required for the further benefit of  its clients.
         IBLAW provides solid experience in established areas of practice, with up-to-date knowledge of
